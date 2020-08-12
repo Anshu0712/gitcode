e044c72baadc354593165509baa11e70bee89499
Anshu0712 <anshunagar1991@gmail.com>
2020-08-12 18:33:35 +0530

    Final commit to track if only modified change is shown in logs

--- Minus.java
+++ Minus.java
@@ -1,3 +1,6 @@
 Second file edited from vi which need not to be commited in the latest version
 
 File edited from GUI!!!!
+
+to track what changes are shown in log of commit after single modification
+
